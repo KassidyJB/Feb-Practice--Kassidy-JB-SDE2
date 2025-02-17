1e1dd62518d67c695909f3fe15995ac32ded86fc
KassidyJB <jeanbartkassidy51@gmail.com>
2025-02-17 11:25:00 +0000

    P2--attempted and done

--- P2.py
+++ P2.py
@@ -14,7 +14,8 @@ perform the conversion.
 temp = input(f'''Which temperature scale 
 would you like to convert from? 
 Celcius? Fahrenheit? or Kelvin?\n>''')
-
+#Creating a select function to control users input and what to 
+#respond with accordingly
 def select():
     if temp == "Celsius":
         print(C)
@@ -22,20 +23,40 @@ def select():
         print(F)
     elif temp == "Kelvin":
         print(K)
-pass
+select()
 
+#Converting celcius to kelvin or fahren based on input
 def C():
     cel = input(f'''What temp scale would you 
     like to convert to? K/F? \n>''')
-    if 
+    if cel == "K":
+        new_cel = {cel}+273.15
+    elif cel == "F":
+        new_cel = ({cel}*9/5) + 32
 
-pass
+    return new_cel#returning the new variable and reassigning after
+    #fully confverting
 
+#Converting fahren to Kelvin or celcius based on input
 def F():
     fah = input(f'''What temp scale would you 
     like to convert to? C/K? \n>''')
-pass
+    if fah == "C":
+        new_fah = ({fah} - 32)*5/9
+    elif fah == "K":
+        new_fah = ({fah} - 32)* 5/9 + 273.15 #setting & assigning all computations
+        #to a new variable
+    return new_fah
+
+
+
 
 def K():
     kel = input(f'''What temp scale would you 
-    like to convert to? C/F? \n>''')
+    like to convert to? C/F? \n>''')#based on user input in select function,
+    #there'll be a more specific quesiton
+    if kel == "C":
+        new_kel = {kel} - 273.15 
+    elif kel == "F":
+        new_kel = ({kel} - 273.15)* 9/5 + 32
+    return new_kel
